3ac947e23df0d9f1b52dd369177bc15e326c22d7
Joel <jyucharico@gmail.com>
2024-10-18 12:22:03 -0300

    Borrado la app contacto

--- apps/contacto/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView, ListView # Clases
-
-class contacto(TemplateView): # Clases
-    template_name = 'contacto/contacto.html'
-    
