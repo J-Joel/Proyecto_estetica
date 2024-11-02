0078f2e9304d9307f5fe588283291fbead32bef7
Joel <jyucharico@gmail.com>
2024-11-01 22:53:36 -0300

    Despligue exitoso

--- ProyectoEstatica/settings/local.py
+++ ProyectoEstatica/settings/local.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
