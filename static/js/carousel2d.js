44b8fdda513e1f2ef151960bedd31f991e0e8ef6
Joel <jyucharico@gmail.com>
2024-11-08 04:54:28 -0300

    Correcion

--- static/js/carousel2d.js
+++ static/js/carousel2d.js
@@ -1,19 +1,6 @@
 (function ($) {
     "use strict";
 
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
-
     $('#product-carousel').owlCarousel({
         autoplay: false,
         autoplayHoverPause: true, // Pausa al pasar el ratón por encima
