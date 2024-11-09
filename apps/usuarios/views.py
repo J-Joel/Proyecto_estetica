72ba0314efa1aa82a3fc57eea5cf8ac51fa8ae6a
CarlaCarpi <carlavc.86@gmail.com>
2024-11-09 13:11:19 -0300

    boton ing y reg

--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render # Funciones
 from django.views.generic import TemplateView, ListView # Clases
+from django.shortcuts import render, redirect
 
 # Create your views here.
 #def login(request):
 #    return render(request,'usuarios/login.html')
 class Login(TemplateView):
     template_name = 'usuarios/login.html'
+    
