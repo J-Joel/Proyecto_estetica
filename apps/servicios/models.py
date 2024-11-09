9d645ddfa7cacd1ed852936b900c588e956a371b
Taliaf6 <178300131+Taliaf6@users.noreply.github.com>
2024-11-09 18:29:54 -0300

    Modificación de Servicios para  Descripción

--- apps/servicios/models.py
+++ apps/servicios/models.py
@@ -1,12 +1,18 @@
 from django.db import models
+from django.shortcuts import render
 
 # Create your models here.
 class Servicio(models.Model):
     id = models.AutoField(primary_key=True)
-    nombre = models.CharField('Nombre',max_length=50, blank = True)
-    descripcion = models.TextField('Descripción',max_length=100)
-    duracion = models.IntegerField() # Duración en minutos o Horas
+    nombre = models.CharField('Nombre',max_length=100, blank = True)
+    descripcion = models.TextField('Descripción',max_length=5000)
+    duracion = models.IntegerField('Duración (en horas)') # Duración en minutos o Horas
     precio = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.nombre
+
+    def listar_servicios(request):
+        # Obtiene todos los servicios de la base de datos
+        servicios = Servicio.objects.all()
+        return render(request, 'listar_servicios.html', {'servicios': servicios})
